c5ddccc22ded0867734ba371988542aaf8817c8c
Ibrahim <ibraguim.karsamov@gmail.com>
2023-04-18 14:32:29 +0200

    remove button + read mark

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-/*
-const bookObject = {
-  title: 'Title : The Lord of the Rings',
-  author: 'Author : J. R. R. Tolkien',
-  genre: 'Genre : High fantasy, Adventure',
-  publicationDate: 'Publication date : 1954',
-  numberOfPages: 'Number of pages : Thousands',
-  language: 'English'
-}
-*/
-
 let myLibrary = [];
 
-document.querySelector('form').addEventListener('submit', (event) => {
+ document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
 
   const bookTitle = document.getElementById('book-title').value;
   const bookAuthor = document.getElementById('book-author').value;
   const bookGenre = document.getElementById('book-genre').value;
-  const isRead = document.getElementById('read-checkbox').checked;
+  const isRead = document.getElementById('book-status').checked;
 
   const addedBook = new Book(bookTitle, bookAuthor, bookGenre, isRead);
 
@@ -34,30 +23,52 @@ document.querySelector('form').addEventListener('submit', (event) => {
   addedBookGrid.classList.add('column');
 
   const addedBookGridTitleSpan = document.createElement('span');
-  addedBookGridTitleSpan.textContent = bookTitle;
+  addedBookGridTitleSpan.textContent = `Title : ${bookTitle}`;
   addedBookGridTitleSpan.classList.add('book-title');
 
   const addedBookGridAuthorSpan = document.createElement('span');
-  addedBookGridAuthorSpan.textContent = bookAuthor;
+  addedBookGridAuthorSpan.textContent = `Author : ${bookAuthor}`;
   addedBookGridAuthorSpan.classList.add('book-author');
 
   const addedBookGridGenreSpan = document.createElement('span');
-  addedBookGridGenreSpan.textContent = bookGenre;
+  addedBookGridGenreSpan.textContent = `Genre : ${bookGenre}`;
   addedBookGridGenreSpan.classList.add('book-genre');
 
+  const addedBookGridStatusLabel = document.createElement('label');
+  const addedBookGridStatusInput = document.createElement('input');
+  addedBookGridStatusLabel.textContent = 'Mark as read';
+  addedBookGridStatusInput.type = 'checkbox';
+  addedBookGridStatusLabel.htmlFor = 'book-status';
+  addedBookGridStatusInput.classList.add('book-status');
+  addedBookGridStatusInput.id = 'book-status';
+
+  if (isRead) {
+    addedBookGridStatusInput.checked = true;
+  } else {
+    addedBookGridStatusInput.checked = false
+  }
+
   const addedBookGridRemoveButton = document.createElement('button');
+  addedBookGridRemoveButton.classList.add('remove-button');
   addedBookGridRemoveButton.textContent = 'Remove';
   addedBookGridRemoveButton.type = 'button';
-  addedBookGridRemoveButton.classList.add('remove-button');
 
   document.querySelector('.books').appendChild(addedBookGrid);
   addedBookGrid.appendChild(addedBookGridTitleSpan);
   addedBookGrid.appendChild(addedBookGridAuthorSpan);
   addedBookGrid.appendChild(addedBookGridGenreSpan);
+  addedBookGrid.appendChild(addedBookGridStatusLabel);
+  addedBookGridStatusLabel.appendChild(addedBookGridStatusInput);
   addedBookGrid.appendChild(addedBookGridRemoveButton);
+
+  const removeButtons = document.querySelectorAll('.remove-button');
+
+  removeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.parentElement.remove();
+    })
+  })
 })
-
-
 
 function Book(title, author, genre, isRead) {
   this.title = title;
