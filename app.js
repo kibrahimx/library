b65598eda4c8a4883d2a46f62c9c354c08c3ca2f
Ibrahim <ibraguim.karsamov@gmail.com>
2023-05-05 10:08:42 +0200

    final script

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 let myLibrary = [];
 
- document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
+const form = document.querySelector('form');
+const newBookButton = document.getElementById('new-book-button');
+const submitButton = document.getElementById('submit-book-button');
+let formScale = false;
+
+function Book(title, author, genre, isRead) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.isRead = isRead;
+}
+
+form.addEventListener('submit', (event) => {
 
   const bookTitle = document.getElementById('book-title').value;
   const bookAuthor = document.getElementById('book-author').value;
   const bookGenre = document.getElementById('book-genre').value;
   const isRead = document.getElementById('book-status').checked;
 
+  event.preventDefault();
+
   const addedBook = new Book(bookTitle, bookAuthor, bookGenre, isRead);
 
   myLibrary.push(addedBook);
-
-  console.log(myLibrary);
-
-  console.log(`Title : ${bookTitle}, Author : ${bookAuthor}, Genre : ${bookGenre}, isRead ${isRead}.`)
 
   const addedBookGrid = document.createElement('div');
 
@@ -34,18 +43,17 @@ let myLibrary = [];
   addedBookGridGenreSpan.textContent = `Genre : ${bookGenre}`;
   addedBookGridGenreSpan.classList.add('book-genre');
 
-  const addedBookGridStatusLabel = document.createElement('label');
-  const addedBookGridStatusInput = document.createElement('input');
-  addedBookGridStatusLabel.textContent = 'Mark as read';
-  addedBookGridStatusInput.type = 'checkbox';
-  addedBookGridStatusLabel.htmlFor = 'book-status';
-  addedBookGridStatusInput.classList.add('book-status');
-  addedBookGridStatusInput.id = 'book-status';
-
+  const addedBookGridStatusButton = document.createElement('button');
+  addedBookGridStatusButton.type = 'button';
+  addedBookGridStatusButton.classList.add('book-status-button');
+  addedBookGridStatusButton.id = 'book-status-button';
+  
   if (isRead) {
-    addedBookGridStatusInput.checked = true;
-  } else {
-    addedBookGridStatusInput.checked = false
+    addedBookGridStatusButton.textContent = 'Read';
+    addedBookGridStatusButton.style.backgroundColor = 'green';
+  } else if (!isRead) {
+    addedBookGridStatusButton.textContent = 'Not read';
+    addedBookGridStatusButton.style.backgroundColor = 'red';
   }
 
   const addedBookGridRemoveButton = document.createElement('button');
@@ -57,8 +65,7 @@ let myLibrary = [];
   addedBookGrid.appendChild(addedBookGridTitleSpan);
   addedBookGrid.appendChild(addedBookGridAuthorSpan);
   addedBookGrid.appendChild(addedBookGridGenreSpan);
-  addedBookGrid.appendChild(addedBookGridStatusLabel);
-  addedBookGridStatusLabel.appendChild(addedBookGridStatusInput);
+  addedBookGrid.appendChild(addedBookGridStatusButton);
   addedBookGrid.appendChild(addedBookGridRemoveButton);
 
   const removeButtons = document.querySelectorAll('.remove-button');
@@ -68,15 +75,39 @@ let myLibrary = [];
       button.parentElement.remove();
     })
   })
+
+  const statusButtons = document.querySelectorAll('.book-status-button');
+
+  statusButtons.forEach(button => {
+    if (!button.hasAttribute('hasClickEventListener')) {
+      button.addEventListener('click', () => {
+        button.setAttribute('hasClickEventListener', 'true');
+        console.log('click status');
+        if (button.textContent == 'Read' && button.style.backgroundColor == 'green') {
+          console.log('read');
+          button.textContent = 'Not read';
+          button.style.backgroundColor = 'red';
+        } else if (button.textContent == 'Not read' && button.style.backgroundColor == 'red') {
+          console.log('not read');
+          button.textContent = 'Read';
+          button.style.backgroundColor = 'green';
+        }
+      })
+    }
+  })
 })
 
-function Book(title, author, genre, isRead) {
-  this.title = title;
-  this.author = author;
-  this.genre = genre;
-  this.isRead = isRead;
-}
+newBookButton.addEventListener('click', () => {
+  if (!formScale) {
+    formScale = true;
+    form.style.scale = '1';
+  } else if (formScale) {
+    formScale = false;
+    form.style.scale = '0';
+  }
+})
 
-function addBookToLibrary() {
-
-}
+submitButton.addEventListener('click', () => {
+  formScale = false;
+  form.style.scale = '0';
+})
